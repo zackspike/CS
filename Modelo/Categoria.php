4848b2cbc3c463b35f805334fd7250ccc97fa34c
zack <isaacpechherrera@gmail.com>
2025-11-17 18:25:08 -0600

    Agregado diagrama MER y cambios en clases

--- Modelo/Categoria.php
+++ Modelo/Categoria.php
@@ -2,8 +2,42 @@
 
 /**
  *
- * @author Gabriela Puch
+ * @author Isaac Herrera
  */
-interface Categoria {
-    public function tipoCategoria(): string;
+class Categoria {
+    private int $idCategoria;
+    private string $nombre;
+    private string $descripcion;
+    
+    public function __construct(int $idCategoria, string $nombre, string $descripcion) {
+        $this->idCategoria = $idCategoria;
+        $this->nombre = $nombre;
+        $this->descripcion = $descripcion;
+    }
+
+    public function getIdCategoria(): int {
+        return $this->idCategoria;
+    }
+
+    public function getNombre(): string {
+        return $this->nombre;
+    }
+
+    public function getDescripcion(): string {
+        return $this->descripcion;
+    }
+
+    public function setIdCategoria(int $idCategoria): void {
+        $this->idCategoria = $idCategoria;
+    }
+
+    public function setNombre(string $nombre): void {
+        $this->nombre = $nombre;
+    }
+
+    public function setDescripcion(string $descripcion): void {
+        $this->descripcion = $descripcion;
+    }
+
+    
 }
